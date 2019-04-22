92efa622ee5fa56472e4216ab66b4653a397a154
Ashley Miller <asm2fe@virginia.edu>
2019-04-22 20:46:14 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,2 +1,6 @@
 class ChattyStudent < Student
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff.How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
 end
